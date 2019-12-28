3fc6e1a09af54d449cfb4b0e1829d7c1606e888e
woojinc <mrwoojinc@gmail.com>
2019-12-28 17:40:00 -0500

    Update e-283-move-zeros.py

--- coding-problems/python/e-283-move-zeros.py
+++ coding-problems/python/e-283-move-zeros.py
@@ -1,6 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for x in nums:
-            if x == 0:
+        if 0 in nums:
+            for x in range(nums.count(0)):
+                # this works because nums.remove(0) will remove the first zero from the beginning
                 nums.remove(0)
                 nums += [0]
