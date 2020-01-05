71e371d7bbe41f42dd617e4e7b23029af395949f
woojinc <mrwoojinc@gmail.com>
2020-01-04 20:57:16 -0500

    Update files

--- coding-problems/python/e-169-majority-element.py
+++ coding-problems/python/e-169-majority-element.py
@@ -1,7 +1,8 @@
+import collections
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # counter = collections.Counter(nums)
-        counter = Counter(nums)
+        counter = collections.Counter(nums)
         for key, value in counter.items():
             if value > len(nums) / 2:
                 return key
--- coding-problems/python/e-283-move-zeros.py
+++ coding-problems/python/e-283-move-zeros.py
@@ -1,5 +1,5 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes(self, nums) -> None:
         if 0 in nums:
             for x in range(nums.count(0)):
                 # this works because nums.remove(0) will remove the first zero from the beginning
