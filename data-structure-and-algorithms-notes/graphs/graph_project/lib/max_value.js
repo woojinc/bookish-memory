123a6d293cfbc4c612d2db4346342289096be24a
woojinc <mrwoojinc@gmail.com>
2020-01-08 15:38:45 -0500

    Clean code by finding max values among its neighbors

--- data-structure-and-algorithms-notes/graphs/graph_project/lib/max_value.js
+++ data-structure-and-algorithms-notes/graphs/graph_project/lib/max_value.js
@@ -1,26 +1,29 @@
 function maxValue(node, visited=new Set()) {
-
-    _depthFirstRec(node, visited);
-
-    let maxVal = node.val;
-
-    visited.forEach(val => {
-        if (val > maxVal) maxVal = val;
-    });
-
-    return maxVal;
-
-}
-
-function _depthFirstRec(node, visited = new Set()) {
-    if (visited.has(node.val)) return;
+    if (visited.has(node.val)) return -Infinity;
     visited.add(node.val);
-    node.neighbors.forEach(node => {
-        _depthFirstRec(node, visited);
-    });
 
-    // return visited;
+    let maxNeighbors = node.neighbors.map( neighbor => maxValue(neighbor, visited) );
+    return Math.max(node.val, ...maxNeighbors);
+
+    // _depthFirstRec(node, visited);
+
+    // let maxVal = node.val;
+
+    // visited.forEach(val => {
+    //     if (val > maxVal) maxVal = val;
+    // });
+
+    // return maxVal;
+
 }
+
+// function _depthFirstRec(node, visited = new Set()) {
+//     if (visited.has(node.val)) return;
+//     visited.add(node.val);
+//     node.neighbors.forEach(node => {
+//         _depthFirstRec(node, visited);
+//     });
+// }
 
 module.exports = {
     maxValue
