82073bd1a92fd2ff978fb625894002dfd000d22e
woojinc <mrwoojinc@gmail.com>
2020-01-13 19:40:24 -0500

    Remove unnecessary logic

--- coding-problems/python/m-46-permutations.py
+++ coding-problems/python/m-46-permutations.py
@@ -1,11 +1,11 @@
 def permute(nums):
     if len(nums) <= 1:
         return [nums]
-    if len(nums) == 2:
-        return [[nums[0], nums[1]], [nums[1], nums[0]]]
+    # if len(nums) == 2:
+    #     return [[nums[0], nums[1]], [nums[1], nums[0]]]
     output = []
     for idx in range(len(nums)):
-        permutations = permute(nums[0:idx] + nums[idx+1:])
+        permutations = permute(nums[:idx] + nums[idx+1:])
         
         for permuation in permutations:
             output.append([nums[idx]] + permuation)
