55a565c27e8e8fe3d83365e4ddcf802bea7041c2
woojinc <mrwoojinc@gmail.com>
2020-01-13 19:35:43 -0500

    Solve using recursion

---
+++ coding-problems/python/m-46-permutations.py
@@ -0,0 +1,16 @@
+def permute(nums):
+    if len(nums) <= 1:
+        return [nums]
+    if len(nums) == 2:
+        return [[nums[0], nums[1]], [nums[1], nums[0]]]
+    output = []
+    for idx in range(len(nums)):
+        permutations = permute(nums[0:idx] + nums[idx+1:])
+        
+        for permuation in permutations:
+            output.append([nums[idx]] + permuation)
+
+    return output
+
+
+print(permute([1,2,3,4]))
